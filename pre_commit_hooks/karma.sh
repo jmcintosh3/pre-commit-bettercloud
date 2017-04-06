5c136a22bb7ad033b67fd384555e6f86a96566ce
McIntosh <john.mcintosh@bettercloud.com>
2017-04-06 14:07:16 -0400

    Update grabbing test result and log to see failure

--- pre_commit_hooks/karma.sh
+++ pre_commit_hooks/karma.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 red="\033[0;31m"
 yellow="\033[1;33m"
 green="\033[1;32m"
 reset="\033[0m"
 
+echo "changed to client"
 cd client/
 echo "changed to client"
 npm test
+testResults=$?
 echo "ran tests"
 cd ../
 echo "changed directory back"
@@ -14,12 +16,11 @@ echo "changed directory back"
 
 # now if tests failed let's abort commit by "exit 1"
 # if not, congratulations, commit is now in Git
-testResults=$?
-if [ $testResults -eq 1 ]
-then
-    echo -e "${red}\n Tests FAILED\n\n commit ABORTED${reset}"
+if [ $testResults -ne 0 ]
+ then
+    echo "${red}\n Tests FAILED\n\n commit ABORTED${reset}"
     exit 1
 else
-    echo -e "${green}\nOK\n${reset}"
+    echo "${green}\nOK\n${reset}"
 fi
 exit 0
